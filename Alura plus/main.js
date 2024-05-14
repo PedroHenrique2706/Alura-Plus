84eba521e4ece8259ad3f3f26c0958a71972f53e
Pedro Henrique <125569169+PedroHenrique2706@users.noreply.github.com>
2024-05-13 22:01:51 -0300

    Inserindo arquivos de melhorias alura-plus

---
+++ Alura plus/main.js
@@ -0,0 +1,25 @@
+
+
+/* 
+const nome = prompt('Qual o seu nome ?')
+const idade = prompt('Qual a sua idade ?')
+const linguagem = prompt('Qual linguagem a sua linguagem preferida ?')
+
+const msg = `Olá ${nome}, você tem ${idade} anos de idade, e a sua linguagem preferida é ${linguagem}`
+
+alert(msg)
+
+const ask = prompt(`Você gosta de programar na linguagem ${linguagem} ? Digite 1 para SIM, e 2 caso a sua reposta seja NÃO.`)
+
+if(ask == 1){
+    alert(`Muito bem, mantenha a consistência, e não desista!`)
+}
+else{
+    alert(`Que pena que você não curtiu muito programar em ${linguagem}, já tentou praticar outra linguagem ?`)
+}
+
+
+
+
+
+/* Perguntar nome, idade, linguagem, e criar uma mensagem. Se gosta de uma linguagem 1 sim, 2 não */
